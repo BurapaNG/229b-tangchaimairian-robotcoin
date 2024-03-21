911cced079e862a2bf1b241840b7436d8ddb979a
BurapaNG <burapa.ngam@bumail.net>
2024-03-21 19:58:47 +0700

    Unknown

---
+++ Assets/Scripts/Magnet.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+
+    public GameObject coinDectectorObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinDectectorObj = GameObject.FindGameObjectWithTag("Coin Detector");
+        coinDectectorObj.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(ActivateCoin());
+            Destroy(transform.GetChild(0).gameObject);
+        }
+    }
+
+    IEnumerator ActivateCoin()
+    {
+        coinDectectorObj.SetActive(true);
+        yield return new WaitForSeconds(10f);
+        coinDectectorObj.SetActive(false);
+    }
+}
+
