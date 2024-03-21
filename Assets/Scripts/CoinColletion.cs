2a39a09b8d76cc1eee310fcff224a92accbc7937
BurapaNG <burapa.ngam@bumail.net>
2024-03-21 18:35:39 +0700

    Add UpdateCoinColletion

--- Assets/Scripts/CoinColletion.cs
+++ Assets/Scripts/CoinColletion.cs
@@ -1,18 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CoinColletion : MonoBehaviour
 {
 
     private int Coin = 0;
+    public TextMeshProUGUI cointext;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Coin")
         {
             Coin++;
+            cointext.text = "Coin: " + Coin.ToString();
             Debug.Log(Coin);
             Destroy(other.gameObject);
         }
