efc543740bdd00fb4ab399c25869bfec53beb616
NDInfinitylove <tanapat.boonr@bumail.net>
2024-03-23 11:50:43 +0700

    AddIceSkate

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private bool groundCheck = false;
-    
 
     public float speed = 7.0f;
     public float jumpForce = 6.0f;
@@ -14,32 +10,28 @@ public class PlayerMovement : MonoBehaviour
     private float forwardInput;
 
     private Rigidbody playerRb;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
         playerRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        
+
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
-        if (groundCheck) 
+        if (groundCheck)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                 groundCheck = false;
             }
-            
         }
-
-        
     }
 
     private void OnCollisionEnter(Collision other)
@@ -47,7 +39,32 @@ public class PlayerMovement : MonoBehaviour
         if (other.gameObject.tag == "Ground")
         {
             groundCheck = true;
-            
         }
+    }
+
+    private void FixedUpdate()
+    {
+        SmoothMovement();
+    }
+
+    private void SmoothMovement()
+    {
+        // สร้างเวกเตอร์เริ่มต้นที่ชี้ไปด้านล่างของตัวละคร
+        Vector3 downwardDirection = -transform.up;
+
+        // สร้างเรนเดอร์เรียนเสียดทาน
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, downwardDirection, out hit))
+        {
+            // หากเรนเดอร์ชนกับพื้น
+            if (hit.distance < 0.1f) // ใช้ค่าความลื่นตามที่คุณต้องการ
+            {
+                // ปรับตำแหน่งของตัวละครให้เรียบบนพื้น
+                transform.position = new Vector3(transform.position.x, hit.point.y + 0.1f, transform.position.z);
+            }
+        }
+
+        // เคลื่อนที่ตัวละครไปข้างหน้า
+        playerRb.MovePosition(transform.position + transform.forward * speed * Time.fixedDeltaTime);
     }
 }
